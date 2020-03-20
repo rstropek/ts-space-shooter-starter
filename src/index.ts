7fe124e0b58011b9bccced8ac68b6a1a58a7f171
Rainer Stropek <rainer@software-architects.at>
2020-03-20 11:14:17 +0100

    Move spaceship

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Scene, CANVAS, Game, GameObjects } from 'phaser';
+import { Scene, Types, CANVAS, Game, GameObjects } from 'phaser';
+
+/** Possible movement directions */
+export enum Direction {
+    Left = -1, 
+    Right = 1 
+}
 
 /**
  * Space shooter scene
@@ -8,6 +14,8 @@ import { Scene, CANVAS, Game, GameObjects } from 'phaser';
  */
 class ShooterScene extends Scene {
     private spaceShip: GameObjects.Image;
+    private speed: number;
+    private cursors: Types.Input.Keyboard.CursorKeys;
 
     preload() {
         // Preload images so that we can use them in our game
@@ -15,6 +23,8 @@ class ShooterScene extends Scene {
         this.load.image('bullet', 'images/scratch-laser.png');
         this.load.image('ship', 'images/scratch-spaceship.png');
         this.load.image('meteor', 'images/scratch-meteor.png');
+
+        this.speed = Phaser.Math.GetSpeed(200, 1);
     }
 
     create() {
@@ -28,6 +38,28 @@ class ShooterScene extends Scene {
         // Position the spaceship horizontally in the middle of the screen
         // and vertically at the bottom of the screen.
         this.spaceShip.setPosition(this.game.canvas.width / 2, this.game.canvas.height * 0.9);
+
+        // Setup game input handling
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+    
+    update(_, delta: number) {
+        // Move ship if cursor keys are pressed
+        if (this.cursors.left.isDown) {
+            this.move(delta, Direction.Left);
+        }
+        else if (this.cursors.right.isDown) {
+            this.move(delta, Direction.Right);
+        }
+    }
+    
+    move(delta: number, direction: Direction) {
+        // Change position
+        this.spaceShip.x += this.speed * delta * direction;
+
+        // Make sure spaceship cannot leave world boundaries
+        this.spaceShip.x = Phaser.Math.Clamp(this.spaceShip.x, this.spaceShip.width / 2,
+            this.game.canvas.width - this.spaceShip.width / 2);
     }
 }
 
