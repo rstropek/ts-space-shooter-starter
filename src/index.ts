f91df134e08104aab4ebebc051051a96ec97411b
Rainer Stropek <rainer@software-architects.at>
2020-03-20 11:07:37 +0100

    Start game and add background

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-console.log('Here we have to add our space shooter code...');
+import { Scene, CANVAS, Game } from 'phaser';
 
-window.alert('You will see your space shooter here!');
+/**
+ * Space shooter scene
+ * 
+ * Learn more about Phaser scenes at 
+ * https://photonstorm.github.io/phaser3-docs/Phaser.Scenes.Systems.html.
+ */
+class ShooterScene extends Scene {
+    preload() {
+        // Preload images so that we can use them in our game
+        this.load.image('space', 'images/deep-space.jpg');
+        this.load.image('bullet', 'images/scratch-laser.png');
+        this.load.image('ship', 'images/scratch-spaceship.png');
+        this.load.image('meteor', 'images/scratch-meteor.png');
+    }
+
+    create() {
+        //  Add a background
+        this.add.tileSprite(0, 0, this.game.canvas.width, this.game.canvas.height, 'space').setOrigin(0, 0);
+    }
+}
+
+const config = {
+    type: CANVAS,
+    width: 512,
+    height: 512,
+    scene: [ShooterScene],
+    physics: { default: 'arcade' },
+    audio: { noAudio: true }
+};
+
+new Game(config);
